7a71ac219c32da4dc06a07f873f2422510de5dee
highTower.SU <WebCD@online.de>
2019-10-28 23:20:32 +0100

    Update node.lua

--- node.lua
+++ node.lua
@@ -1,16 +1,20 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
-node.make_nested() -- so we can access child directories
+local font = resource.load_font "Ubuntu-C.ttf"
+
+local alarmtext = "Noch keine Einsatzinformationen!"
+
 
 util.json_watch("config.json", function(config)
-    print "new top level config"
-    pp(config)
+    alarmtext=config.text
 end)
 
-util.json_watch("automated/config.json", function(config)
-    print "new automated json config!"
-    pp(config)
-end)
+local function draw_info()
+    local s = HEIGHT/10
+    font:write(s, s*0.5, alarmtext, s, 1,1,1,1)
+end
 
 function node.render()
+    gl.clear(0,0,0,1)
+    draw_info()
 end
